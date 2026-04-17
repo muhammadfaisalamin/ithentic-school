e3d8d1625b2718697154f4dfc4dcd63bdc8b9f40
stmikbjb2025 <stmikbjb2025@gmail.com>
2026-04-17 10:39:17 +0800

    feat: implement responsive layout structure for AdminPage component

--- src/app/(dashboard)/admin/page.tsx
+++ src/app/(dashboard)/admin/page.tsx
@@ -1,3 +1,11 @@
 export default function AdminPage() {
-	return <div>AdminPage</div>;
+	return (
+		// STRUKTUR DASAR KONTEN
+		<div className="p-4 flex gap-4 flex-col md:flex-row">
+			{/* LEFT */}
+			<div className="w-full lg:w-2/3">l</div>
+			{/* RIGHT */}
+			<div className="w-full lg:w-1/3">r</div>
+		</div>
+	);
 }
