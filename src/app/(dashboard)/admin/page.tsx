2ca04f72884aab5687ebfa1182b351c90ac9721b
stmikbjb2025 <stmikbjb2025@gmail.com>
2026-04-17 16:10:43 +0800

    feat: implement UserCard component and integrate into AdminPage layout

--- src/app/(dashboard)/admin/page.tsx
+++ src/app/(dashboard)/admin/page.tsx
@@ -1,9 +1,19 @@
+import UserCard from '@/app/components/UserCard';
+
 export default function AdminPage() {
 	return (
 		// STRUKTUR DASAR KONTEN
 		<div className="p-4 flex gap-4 flex-col md:flex-row">
 			{/* LEFT */}
-			<div className="w-full lg:w-2/3">l</div>
+			<div className="w-full lg:w-2/3">
+				{/* USER CARDS */}
+				<div className="">
+					<UserCard type="student" />
+					<UserCard type="teacher" />
+					<UserCard type="parent" />
+					<UserCard type="staff" />
+				</div>
+			</div>
 			{/* RIGHT */}
 			<div className="w-full lg:w-1/3">r</div>
 		</div>
